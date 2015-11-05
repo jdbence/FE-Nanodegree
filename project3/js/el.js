a0798905de512035aa98d5ae531b5f02e1460f41
jdbence <jdbence@yahoo.com>
2015-11-05 20:52:24 +0000

    added documentation for el class

--- project3/js/el.js
+++ project3/js/el.js
@@ -7,6 +7,11 @@ var El = (function() {
     var instance = function(){
     };
     
+    /**
+    * @description Removes the className from each element in the array
+    * @param {array} elements - Array of DOM elements
+    * @param {string} className - Class to remove
+    */
     instance.removeClass = function(elements, className){
         var i, c;
         if(elements){
@@ -20,6 +25,11 @@ var El = (function() {
         }
     }
     
+    /**
+    * @description Adds the className on each element in the array
+    * @param {array} elements - Array of DOM elements
+    * @param {string} className - Class to add
+    */
     instance.addClass = function(elements, className){
         var i;
         
@@ -35,6 +45,12 @@ var El = (function() {
         }
     }
     
+    /**
+    * @description Adds a listener on the element
+    * @param {array} elements - Array of DOM elements
+    * @param {string} event - Event to listen for
+    * @param {function} callback - Function to call when the event is captured
+    */
     instance.addListener = function(elements, event, callback){
         var i;
         
@@ -48,12 +64,20 @@ var El = (function() {
         }
     }
     
-    instance.show = function(element){
-        this.removeClass(document.getElementsByClassName(element), "hidden");
+    /**
+    * @description Removes the hidden class from the elements with the given className
+    * @param {string} className - The elements to find
+    */
+    instance.show = function(className){
+        this.removeClass(document.getElementsByClassName(className), "hidden");
     }
     
-    instance.hide = function(element){
-        this.addClass(document.getElementsByClassName(element), "hidden");
+    /**
+    * @description Adds the hidden class on the elements with the given className
+    * @param {string} className - The elements to find
+    */
+    instance.hide = function(className){
+        this.addClass(document.getElementsByClassName(className), "hidden");
     }
     
     return instance;
